4ff76b32cde34137117b3fd371eba0c5e88933b3
amuseDave <amusedave2@gmail.com>
2025-06-09 14:14:19 +0300

    create get element function

--- utils.js
+++ utils.js
@@ -2,3 +2,9 @@ export function getRandomElement(arr) {
   const idx = Math.floor(Math.random * arr.length);
   return [arr[idx], idx];
 }
+
+export function getElement(arr, element) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) return [arr[i], i];
+  }
+}
