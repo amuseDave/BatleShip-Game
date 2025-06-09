e22df964529e55bd179887c431bb601578c3b48d
amuseDave <amusedave2@gmail.com>
2025-06-09 15:47:10 +0300

    create copy grid utility function

--- utils.js
+++ utils.js
@@ -8,3 +8,13 @@ export function getElement(arr, element) {
     if (arr[i] === element) return [arr[i], i];
   }
 }
+
+export function copyGrid(grid) {
+  const copy = [...grid];
+
+  for (let i = 0; i < copy.length; i++) {
+    copy[i][1] = new Set(copy[i][1]);
+  }
+
+  return copy;
+}
